aa547599c7029e8e108293d35d589e9f9f1d8d4c
Zorehan <kadargergo20@gmail.com>
2024-09-05 11:44:04 +0200

    vi hygger I guess

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,74 @@
-import { ModCallback } from "isaac-typescript-definitions";
-import { getNPCs, getPlayers, getRandomInt } from "isaacscript-common";
+import { ModCallback, type FamiliarVariant } from "isaac-typescript-definitions";
+import { getNPCs, getPlayers, getRandomInt, ModCallbackCustom } from "isaacscript-common";
 
 const MOD_NAME = "Gergelations";
 const GREEN_CANDLE_COLLECTIBLE_TYPE = Isaac.GetItemIdByName("Green Candle");
 const ANGELIC_BABY_COLLECTIBLE_TYPE = Isaac.GetItemIdByName("Angelic Baby");
 
+
+const ANGELIC_BABY_VARIANT = Isaac.GetEntityVariantByName("Angelic Baby");
+
+let angelicBaby: Entity | null = null;
+
+let angelicBabyPlayMovie = -1;
+let entities: Entity[] = [];
+
+let chestPickupCount = 0;
+
+const playerDistance = 90;
+const pickupDistance = 150;
+const collisionDistance = 30;
+
+let modRunning = false;
+
+
 export function main() {
   const mod = RegisterMod(MOD_NAME, 1);
 
   mod.AddCallback(ModCallback.POST_UPDATE, postUpdate);
+  mod.AddCallback(ModCallback.POST_UPDATE, postUpdateforAngelicBaby);
+  mod.AddCallback(ModCallback.POST_FAMILIAR_UPDATE(angelicbabyUpdate, ANGELIC_BABY_VARIANT))
 
   Isaac.DebugString(`${MOD_NAME} initialized.`);
 }
 
 function postUpdate() {
   checkApplyGreenCandleEffect();
+}
+
+function postUpdateforAngelicBaby() {
+  let player = Isaac.GetPlayer(0);
+  if (player.HasCollectible(ANGELIC_BABY_COLLECTIBLE_TYPE)) {
+    entities = Isaac.GetRoomEntities();
+
+    if (angelicBaby == null) {
+      for (let ent = 0; ent < entities.length; ent++) {
+        let entity = entities[ent];
+        if (entity && entity.Type === 3 && entity.Variant === ANGELIC_BABY_VARIANT) {
+          angelicBaby = entity;
+        }
+      }
+    }
+  }
+
+  if (!player.HasCollectible(ANGELIC_BABY_COLLECTIBLE_TYPE) && angelicBaby !== null) {
+    angelicBaby.Remove();
+    angelicBaby = null;
+  }
+}
+
+function angelicbabyUpdate(Entity: FamiliarVariant)
+{
+let player = Isaac.GetPlayer(0);
+
+let givestuff = true;
+if(chestPickupCount / 2 != 0)
+{
+  givestuff = false;
+}
+let chasing = false;
+
+let Sprite =
 }
 
 function checkApplyGreenCandleEffect() {
