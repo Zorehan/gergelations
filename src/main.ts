62beef82cc8751c657eec7b1f7a3f1fffeaff4af
Zorehan <kadargergo20@gmail.com>
2024-09-05 09:33:10 +0200

    banjelic baby animation

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,41 @@
 import { ModCallback } from "isaac-typescript-definitions";
 import { getNPCs, getPlayers, getRandomInt } from "isaacscript-common";
 
-const MOD_NAME = "gergelations";
+const MOD_NAME = "Gergelations";
 const GREEN_CANDLE_COLLECTIBLE_TYPE = Isaac.GetItemIdByName("Green Candle");
+const ANGELIC_BABY_COLLECTIBLE_TYPE = Isaac.GetItemIdByName("Angelic Baby");
 
-
-
-export function main(): void {
-
+export function main() {
   const mod = RegisterMod(MOD_NAME, 1);
+
   mod.AddCallback(ModCallback.POST_UPDATE, postUpdate);
 
-  // Print a message to the "log.txt" file.
   Isaac.DebugString(`${MOD_NAME} initialized.`);
 }
 
-function postUpdate()
-{
+function postUpdate() {
   checkApplyGreenCandleEffect();
-  Isaac.DebugString("A game frame just passed.");
 }
 
-function checkApplyGreenCandleEffect()
-{
-  for (const player of getPlayers())
-  {
-    if(player.HasCollectible(GREEN_CANDLE_COLLECTIBLE_TYPE))
-    {
+function checkApplyGreenCandleEffect() {
+  for (const player of getPlayers()) {
+    if (player.HasCollectible(GREEN_CANDLE_COLLECTIBLE_TYPE)) {
       applyGreenCandleEffect(player);
     }
   }
 }
 
-function applyGreenCandleEffect(player: EntityPlayer)
-{
-  for (const npc of getNPCs())
-  {
-    if(shouldApplyGreenCandleEffectToNPC(npc))
+function applyGreenCandleEffect(player: EntityPlayer) {
+  for (const npc of getNPCs()) {
+    if (shouldApplyGreenCandleEffectToNPC(npc)) {
+      // - The source is the player.
+      // - The duration is 100 frames.
+      // - The damage is equal to the player's damage stat.
       npc.AddPoison(EntityRef(player), 100, player.Damage);
+    }
   }
 }
 
-function shouldApplyGreenCandleEffectToNPC(npc: EntityNPC)
-{
-  return npc.IsVulnerableEnemy() && getRandomInt(1, 100, undefined) === 1;
-}
-function postPlayerInit() {
-  Isaac.DebugString("Callback fired: POST_PLAYER_INIT");
+function shouldApplyGreenCandleEffectToNPC(npc: EntityNPC) {
+  return npc.IsVulnerableEnemy() && getRandomInt(1, 500, undefined) === 1;
 }
